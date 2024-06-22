421530b9c549f208a004c146209c6c95cab5acbc
Jeevan1101 <jkjeevan313@gmail.com>
2024-06-22 11:16:00 +0530

    Update elevator_testbench2.v

--- elevator_testbench2.v
+++ elevator_testbench2.v
@@ -7,7 +7,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module testbench2_elevator_controller();
+module testbench2();
 reg clock , reset , emergency , over_weight , IR_sensor ;
 reg [2:0] req_floor ;
 wire up,down,idle,door,emergency_stop ;
@@ -16,17 +16,18 @@ wire [2:0] current_floor ;
 wire [7:0] requests ;
 
 // module instantiating 
-elevator_design DUT2(clock,reset,req_floor,emergency,over_weight,IR_sensor,up,down,idle,door,max_request,min_request,emergency_stop,current_floor,requests);
+elevator_design DUT_ELEVATOR(clock,reset,req_floor,emergency,over_weight,IR_sensor,up,down,idle,door,max_request,min_request,emergency_stop,current_floor,requests);
 
 initial 
      begin
       clock=1'b0 ;
-      $dumpfile("elevator2.vcd") ;
-      $dumpvars(0,testbench2_elevator_controller) ;
+      $dumpfile("elevatornew.vcd") ;
+      $dumpvars(0,testbench2) ;
      end
 always #5 clock=~clock ;
 initial 
       begin
+        emergency=0 ;
         over_weight=0 ;
         IR_sensor=0 ;
         reset=1 ;
@@ -39,11 +40,21 @@ initial
       #20 req_floor=5 ;
       #20 req_floor=1 ;
       #30 req_floor=6 ;
-      #25 req_floor=3 ;
+      #29 req_floor=3 ;
       #30 req_floor=4 ;
       #50 req_floor=6 ;
+      #50 req_floor=7 ;
+      #50 req_floor=3 ;
+      #50 req_floor=0 ;
       end
-   initial  
+initial 
+        begin
+        #100 over_weight=1 ;
+        #20 over_weight=0 ;
+        #180 over_weight=1 ;
+        #15 over_weight=0 ;
+        end
+initial  
          begin
           $display(" SIMULATION RESULTS OBTAINED : ") ;
           $monitor(" time=%t , clock=%b , reset=%b , req_floor=%h , emergency=%b , IR_sensor=%b , overweight = %b , up=%b , down=%b, idle=%b , door=%b , Emergency_stop=%b , current_floor=%h , min_request=%h , max_request=%h , requests=%h ",$time,clock,reset,req_floor,emergency,IR_sensor,over_weight,up,down,idle,door,emergency_stop,current_floor,min_request,max_request,requests) ;
